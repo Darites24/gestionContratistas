16bc1c62e609caafb1963e204647f4bb77be6b2e
Darites24 <juanjolopez2402@gmail.com>
2025-03-21 13:44:35 -0500

    Implementación de Login y Logout funcional

--- gestionContratistas/contratistas/urls.py
+++ gestionContratistas/contratistas/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from .views import listarContratistas, crearContratista, editarContratista, eliminarContratista, detalleContratista, crearContrato
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('', listarContratistas, name='listarContratistas'),
+    path('listar/', listarContratistas, name='listarContratistas'),
     path('crear/', crearContratista, name='crearContratista'),
     path('editar/<int:pk>/', editarContratista, name='editarContratista'),
     path('eliminar/<int:pk>/', eliminarContratista, name='eliminarContratista'),
     path('detalle/<int:pk>/', detalleContratista, name='detalleContratista'),
     path('crearContrato/<int:pk>', crearContrato, name='crearContrato'),
+    path('', LoginView.as_view(template_name='contratistas/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gestionContratistas/gestionContratistas/settings.py
+++ gestionContratistas/gestionContratistas/settings.py
@@ -126,3 +126,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/listar/'
+LOGOUT_REDIRECT_URL = '/'
